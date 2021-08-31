c6b2eb3db3105af612679bbf0c32554d587b1456
NidhiKJha <menidhikjha@gmail.com>
2021-08-31 08:33:22 +0530

    added package manager for sass

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.scss';
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,7 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    Concept Title
     </div>
   );
 }
