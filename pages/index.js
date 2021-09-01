bbbdcc2fb6944d569863df6daaa771df48928347
NidhiKJha <menidhikjha@gmail.com>
2021-09-01 11:41:56 +0530

    initial setup done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import Head from "next/head";
+import { createClient } from "contentful";
 
-export default function Home() {
+export async function getStaticProps(){
+  const client = createClient({
+    space:process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  })
+
+  const res = await client.getEntries({
+    content_type : "blog"
+  })
+
+  return{
+    props:{
+      blogs: res.items
+    }
+  }
+}
+
+export default function Home(props) {
+  console.log(props)
   return (
-    <div className="container">
-    Concept Title
+    <div className="container" style={{ textAlign: "center" }}>
+      <h1 className="title" style={{ fontSize: "60px", color: "black" }}>
+        Title
+      </h1>
+      {props.blogs.map((blog) => (
+        <h2 style={{ fontSize: "35px", color: "blueviolet" }} key={blog.sys.id}>
+        {blog.fields.blogTitle}
+      </h2>
+      ))}
+
     </div>
   );
 }
